f57b31717851a1c7973493d75520555c86ef92a2
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-06 22:49:48 +0000

    set

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-#match any pattern that start with h and end with n with a
-#character inbetween
-put ARGV[0].scan(/^h.n$/).join
+#match any pattern that start with h and end with n with
+puts ARGV[0].scan(/^h.n$/).join
