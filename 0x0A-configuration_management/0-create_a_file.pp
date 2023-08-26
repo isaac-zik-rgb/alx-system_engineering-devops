f2e63b43fc4b6017135dad0b246e141f1f00ad20
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-26 16:08:13 +0100

    task0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# a puppet file that write a simple content
+file { '/tmp/school':
+  ensure            => file,
+    path            => '/tmp/school',
+      mode          => '0744',
+        owner       => 'www-data',
+          group     => 'www-data',
+            content => 'I love Puppet',
+            }
