5598b35272fdc410a8a07ce259bf220627c4c04c
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-26 17:02:51 +0100

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using puppet to install flask with the version 2.1.0
-exe { 'flask':
+exec { 'flask':
     command => '/usr/bin/pip3 install flask=2.1.0',
     unless  => '/usr/bin/pip3 install show flask | grep "Version: 2.1.0"',
     }
