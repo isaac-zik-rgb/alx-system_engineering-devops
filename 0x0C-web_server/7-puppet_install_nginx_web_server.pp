3114cc23ec368dca6fa5df77605fa3ab69d48611
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-08-30 22:24:23 +0100

    lasttask

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,52 +1,6 @@
-# Installing and configuring Nginx on an Ubuntu server using Puppet
+# Installs a Nginx server
 
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-  }
-
-# Allowing Nginx through the firewall
-service { 'ufw':
-  ensure => running,
-  }
-
-ufw::allow { 'Nginx HTTP':
-  port => 80,
-  }
-
-# Create our file
-file { '/var/www/html/hello.html':
-  ensure    => 'present',
-    content => 'Hello World!',
-    }
-
-# Configure Nginx server with inline template
-file { '/etc/nginx/sites-available/default':
-  ensure                  => 'present',
-    content               => '
-    server {
-        listen 80;
-            listen [::]:80;
-
-    root /var/www/html;
-        index hello.html;
-
-    server_name localhost;
-
-    location / {
-            try_files $uri $uri/ =404;
-                }
-
-    location /redirect_me {
-            rewrite ^ 301 "https://www.youtube.com/watch?v=QH2-TGUlwu4";
-                }
-                }
-                ',
-                  require => Package['nginx'],
-                  }
-
-# Enable the site and restart Nginx
-exec { 'enable_site_and_restart':
-  command   => 'ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default && service nginx restart',
-    require => [File['/etc/nginx/sites-available/default'], Package['nginx']],
+exec {'install':
+  provider => shell,
+    command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
     }
