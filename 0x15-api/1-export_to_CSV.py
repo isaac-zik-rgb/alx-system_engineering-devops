8598192f5487dd365bd4d58fa0cdace0ee5de1ef
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-25 20:25:28 +0100

    task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Export data in the CSV formate"""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("name")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    with open("{}.csv".format(user_id), mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        [csv_writer.writerow([user_id, username, task.get("completed"),
+                              task.get("title")]) for task in todos]
