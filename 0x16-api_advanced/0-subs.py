64e569ee3ee31ef9c3b95d43da3f3d69e06c27c6
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-10 12:17:17 +0100

    reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns total number of subscribers"""
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     header = {'User-Agent': 'PythonScript/3.0'}
     response = requests.get(url, header)
 
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get('data', {}).get('subscribers', 0)
         return subscribers
     else:
         return 0
