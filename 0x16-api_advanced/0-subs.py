c9e99c8b5b6fdb35ec3940b48d781ab35a6da746
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-10 12:10:03 +0100

    reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and
+returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns total number of subscribers"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    header = {'User-Agent': 'PythonScript/3.0'}
+    response = requests.get(url, header)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """a function that queries the Reddit API and prints the titles"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    header = {'User-Agent': 'PythonScript/3.0'}
+    response = requests.get(url, header)
+    data = response.json()
+    if response.status_code == 200:
+        posts = data.get('data', {}).get('children', [])
+        top_ten_title = []
+        for post in posts[:10]:
+            title = post.get('data', {}).get('title')
+            if title:
+                top_ten_title.append(title)
+        for i, title in enumerate(top_ten_title, 1):
+            print(title)
+    else:
+        print('None')
