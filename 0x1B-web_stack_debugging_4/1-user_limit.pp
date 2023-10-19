d5222a4eebdd61c58d67eca62248cce0df6c0053
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-19 20:06:48 +0100

    adduser holberton

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # create a new ubuntu user called holberton
-exec { 'add new user':
-  command => 'adduser holberton',
-    path  => '/usr/sbin/',
+# Define the 'holberton' user
+user { 'holberton':
+  ensure       => 'present',
+    managehome => true, # Create the user's home directory
     }
+
+user_password { 'holberton':
+  password => '1234',
+  }
