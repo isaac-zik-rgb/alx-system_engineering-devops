5a13d554f72008c494c55e127c12247135fced77
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-19 17:57:07 +0100

    puppet-nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # This script configure nginx ULIMIT to accept concurrence traffic
 exec { 'fix-nginx-config':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path    => ['/usr/local/bin', '/bin'],
+    path    => '/usr/local/bin:/bin/',
     }
 
 # Restarting nginx
 exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',
-    path    => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
-      require => Exec['fix-nginx-config'], # Ensure that nginx is restarted after the config change.
+  command => ' nginx restart',
+    path    => '/etc/init.d/'
+
       }
