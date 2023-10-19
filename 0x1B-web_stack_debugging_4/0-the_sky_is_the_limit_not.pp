7ed01bcdad5011389ade729203d443b649cf8e84
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-10-19 18:41:22 +0100

    puppet-nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 exec { 'fix-nginx-config':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path    => ['/usr/local/bin', '/bin'],
+    path  => ['/usr/local/bin', '/bin'],
     }
 
 # Restarting nginx
 exec { 'nginx-restart':
   command => '/etc/init.d/nginx restart',
-    path    => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
-      require => Exec['fix-nginx-config'], # Ensure that nginx is restarted after the config change.
-      }
+  path    => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
+  require => Exec['fix-nginx-config'], # Ensure that nginx is restarted after the config change.
+  }
